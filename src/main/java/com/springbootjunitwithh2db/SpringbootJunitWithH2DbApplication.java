50cb3f46f5b0855a711e3842f77429c31ff82473
Sushil <sushilsridhar16@gmail.com>
2021-05-27 23:03:05 +0530

    initial commit

---
+++ src/main/java/com/springbootjunitwithh2db/SpringbootJunitWithH2DbApplication.java
@@ -0,0 +1,13 @@
+package com.springbootjunitwithh2db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJunitWithH2DbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJunitWithH2DbApplication.class, args);
+	}
+
+}
