7294900fdf0897182661407286da06902b0e3072
Sushil <sushilsridhar16@gmail.com>
2021-06-01 10:28:29 +0530

    plan for this repository

--- src/main/java/service/AccountService.java
+++ src/main/java/service/AccountService.java
@@ -1,7 +1,21 @@
 package service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import repository.AccountRepository;
 
 @Service
 public class AccountService {
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    public void updateAccountDetails() {
+
+        //find by id, use embedded id
+        //  if already present update, else create new record
+        // use mapstruct lombok , feature flag with @EnableCOnfigurationProperties
+        // custom annotation
+
+    }
 }
