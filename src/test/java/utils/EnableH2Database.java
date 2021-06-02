37a4701fe2bd391d69cc792d4862070f822724fd
Sushil <sushilsridhar16@gmail.com>
2021-06-02 22:21:56 +0530

    custom annotation

--- src/test/java/utils/EnableH2Database.java
+++ src/test/java/utils/EnableH2Database.java
@@ -3,7 +3,14 @@ package utils;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 @ActiveProfiles("test")
 @DataJpaTest
-public interface EnableH2Database {
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface EnableH2Database {
 }
